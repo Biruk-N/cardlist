bc5a17ad6f0798d57453aae826c93bb9a1588261
biruk <birubu178@gmail.com>
2021-11-08 23:28:57 +0300

    mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 // import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
-        
+        <input value='Email Address'/>
+        <Button variant="contained">Log in</Button>
+        <Button variant="contained">Create Account</Button>
     </div>
   );
 }
